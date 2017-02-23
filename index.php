95411235d3bd937af74a9f93c26ec6a64750d3db
FoxyDecoy <royalkeiwurm@gmail.com>
2017-02-23 09:57:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	include_once('twig/lib/Twig/Autoloader.php');
+
+    Twig_Autoloader::register();
+
+    $loader = new Twig_Loader_Filesystem('templates'); // Dossier contenant les templates
+
+    $twig = new Twig_Environment($loader, array(
+      'cache' => false
+    ));
+
+	$tpl = $twig->loadTemplate('index.twig');
+	
+	echo $tpl->render(array(
+        'title' => 'Twig example',
+		'exemple' => 'test',
+		'online' => 1
+    ));
+
+	
