116e571a285a3d9abb8dc2fb6757e172f62b1eda
CedricPEPS <cedric.p@codeur.online>
2017-02-28 13:55:43 +0100

    Update signUp.php

--- controller/signUp.php
+++ controller/signUp.php
@@ -12,26 +12,19 @@
 
 		function create () 
 		{
-			
-			// var_dump($_POST);
+		
 			$pseudo = $_POST['pseudo'];
 			$firstname = $_POST['firstname'];
 			$lastname = $_POST['lastname'];
 			$mail = $_POST['mail'];
 			$password = $_POST['password'];
 
-			// var_dump($pseudo);
-
 			Controller::loadClass('signUpAccount');
+
 			$db = new signUpAccount($pseudo, $firstname, $lastname, $mail, $password);
-			
-			// $user = signUpAccount::insertUser($pseudo, $firstname, $lastname, $mail, $password);
 
 			$db->insertUser($pseudo, $firstname, $lastname, $mail, $password);
-
-
-
-			
+	
 		}
 		
 	}
