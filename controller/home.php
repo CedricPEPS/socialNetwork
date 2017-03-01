cdac736eb322afbd732460a99b119cd3a360865d
HINGRAY <royalkeiwurm@gmail.com>
2017-03-01 11:34:45 +0100

    Add files via upload
    add function search friends

--- controller/home.php
+++ controller/home.php
@@ -3,24 +3,36 @@
 	class home extends Controller {
 
 		function index() {
-			$data = array(
-				'title' => "Home Social NetWork",
-				'asset' => ASSET
-			);
+			if (isset($_SESSION['pseudo'])) {
+				Controller::loadClass('articles');
+				
+				$articles = articles::getArticles($_SESSION["id"]);
 
+				$data = array(
+					'title' => "Home Social NetWork",
+					'asset' => ASSET,
+					'online' => true,
+					'root' => ROOT, 
+					'articles' => $articles
+				);
+			} else {
+				$data = array(
+					'title' => "Home Social NetWork",
+					'asset' => ASSET,
+					'root' => ROOT
+				);
+			}
 			return $data;
 		}
 
 		function log() {
-			extract($_POST);
-
 			Controller::loadClass('user');
 			Controller::loadClass('articles');
+				
 
-			if (isset($_SESSION['pseudo'])) {
-				$articles = articles::getArticles($_SESSION["id"]);
-			} else {
-				$user = log::getUser($login, $password);
+			if (!isset($_SESSION['pseudo'])) {
+				$user = log::getUser($_POST['login'], $_POST['password']);
+				
 				// Set SESSION
 				$_SESSION['pseudo'] = $user['pseudo'];
 				$_SESSION['id'] 	= $user['id'];
@@ -31,36 +43,47 @@
 				$data = array(
 					'title' => 'Profil '.$_SESSION['pseudo'].' ',
 					'asset' => ASSET,
-					'root' => ROOT, 
-					'online' => true,
-					'articles' => $articles
+					'root' => ROOT,
+					'articles' => $articles,
+					'online' => true
 				);
 			} else {
 				$data = array(
 					'online' => false,
 					'root' => ROOT,
 					'asset' => ASSET,
-					'error' => 'Invalid login or password'
+					'error' => 'Invalid login or password',
+					'title' => "Home Social NetWork"
 				);
 			}
-
 			return $data;
 		}
 		
 		function createArticle() {
-			extract($_POST);
-
 			Controller::loadClass('articles');
 
-			$articles = articles::insert($content, $_SESSION["id"]);
-
-			header("Location: ".ROOT."home/log");
+			$articles = articles::insert($_POST['content'], $_SESSION["id"]);
+			header("Location: ".ROOT."home");
 		}
 
-		function disconnect()
+		function search() {
+			Controller::loadClass('user');
 
-		{
+			$find = log::getUserByPseudo($_POST['pseudo']);
+
+			$data = array(
+				'title' => 'Profil '.$_SESSION['pseudo'].' ',
+				'asset' => ASSET,
+				'root' => ROOT, 
+				'online' => true
+			);
+
+			return $data;
+		}
+
+		function disconnect() {
 			session_destroy();
+
 			$data = array(
 				'title' => "Home Social NetWork",
 				'asset' => ASSET,
@@ -69,4 +92,5 @@
 
 			return $data;
 		}
+
 	}
