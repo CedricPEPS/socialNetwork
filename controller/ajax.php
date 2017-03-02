ef77b3c0b5d64f84aec6b7b9f0e058dc37cd7b7a
CedricPEPS <cedric.p@codeur.online>
2017-03-02 16:47:03 +0100

    Fonction Ajax pour verif pseudo dans signUp

---
+++ controller/ajax.php
@@ -0,0 +1,32 @@
+<?php
+class ajax extends Controller{
+
+	function validate () 
+		{
+		
+			$pseudo 		= $_REQUEST['pseudo'];
+			
+
+			$query  = DataBase::bdd()->query("SELECT * FROM users WHERE pseudo ='{$pseudo}'");
+			$fetch  = $query->fetch();
+			$row    = $query->rowCount();
+			
+			if ($row == 0) 
+			{
+				$data = array(
+					'validate'	=> true
+				);
+				return $data;
+			
+			}
+			else
+			{
+				$data = array(
+					'validate'	=>  false
+				);
+				return $data;
+			}
+		}
+
+
+}
