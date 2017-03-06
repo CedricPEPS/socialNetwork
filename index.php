b16a9e49fdf3914c2c26182619f452f0145bc552
HINGRAY <royalkeiwurm@gmail.com>
2017-03-06 10:59:15 +0100

    Add files via upload
    add utils function

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-
 require('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader, [
 	'cache' => false
 ]);
+$twig->getExtension('Twig_Extension_Core')->setTimezone('Europe/Paris');
+
 define('ASSET', "http://localhost/socialNetwork/templates/asset");
 define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
 define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+
 require('core/controller.php');
 
 $params = explode('/', $_GET['p']);
@@ -26,22 +28,18 @@ $controller = new $controller();
 if (method_exists($controller, $action)) {
 	require('core/connect.class.php');
 	require('core/hash.class.php');
+
 	session_start();
+
 	$data = $controller->$action();
-
+	
 	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-	/* special ajax here */
-
-	$data = json_encode($data);
-	echo $data;
-
+		/* special ajax here */
+		$data = json_encode($data);
+		echo $data;
+	} else {
+		echo $twig->render($params[0].'.twig', $data);
 	}
-	else{
-
-	echo $twig->render($params[0].'.twig', $data);
-		
-	}
-
 } else {
 	echo 'erreur 404';
 }
